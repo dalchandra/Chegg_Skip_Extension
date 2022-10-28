7f30b5a6a55608cf41716ada537df07d239affce
Dalchandra Sharma <39084636+dalchandra@users.noreply.github.com>
2022-10-28 16:22:43 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var targetPath = "https://expert.chegg.com/expertqna";
-var exitClassName = "sc-1eq90u-3 sc-1eq90u-7 jaaInW"; // to hide btn when exit is clicked
+var exitClassName = "sc-1eq90u-7"; // to hide btn when exit is clicked
 
 window.addEventListener("load", function(event){
     let currentPath = window.location.href;
@@ -34,6 +34,43 @@ window.addEventListener("click", function(event){
     }else{
         //never reach
     }
+});
+
+var i=1;
+//skip function
+function skip(){
+		if(document.getElementsByClassName("sc-1eq90u-7")[0]==null)///////////Skip Button
+		{
+			window.open("https://expert.chegg.com/expertqna","_self");
+		}
+		else
+		{
+			i=0;
+		    document.getElementsByClassName("sc-1eq90u-7")[0].click() // skip button
+		    let skipOptions = document.getElementsByClassName("q4pgtx-0 dlvpFB");
+		    skipOptions[skipOptions.length - 1].click() ; // to select the skip reason
+            if (document.getElementsByClassName("sc-1eq90u-7 sc-1eq90u-8 ewQEum iAtkV")[0])
+		        document.getElementsByClassName("sc-1eq90u-7 sc-1eq90u-8 ewQEum iAtkV")[0].click()
+            else
+                document.getElementsByClassName("sc-1eq90u-5 sc-1eq90u-6 jfgqtc ibrWOj")[0].click()///For Mobile
+		}
+}
+setTimeout(function(){
+	if(document.getElementsByClassName("sc-1eq90u-7")[0]==null)
+	{
+		if(window.location.href==targetPath)
+			window.open("https://expert.chegg.com/expertqna","_self");
+	}
+	else if(i==1)
+	{
+		i=0;
+		var sound = new Audio;
+		sound.src = "https://assets.mixkit.co/sfx/download/mixkit-arcade-retro-game-over-213.wav";
+		sound.play();
+	}
+},15000);
+
+
 });
 
 var i=1;
